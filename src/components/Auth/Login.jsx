d598ebcc868ddd63d011d23bcf6aa769aadf59dc
Vishal Nema <vishalnema23@gmail.com>
2025-02-20 18:55:58 +0530

    login page updated

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setEmail("");
+    setPassword("");
+  };
   return (
-    <div className='flex h-screen w-full items-center justify-center'>
-      <div className='border-4 border-emerald-600 p-40 rounded-xl'>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="border-4 border-emerald-600 p-40 rounded-xl">
         <form
-         className="flex flex-col items-center justify-center"
-       >
-        <h2 className="text-3xl font-bold text-emerald-600">Login</h2>
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <h2 className="text-3xl font-bold text-emerald-600">Login</h2>
           <p className="text-sm text-gray-400 3">
             Don't have an account?{" "}
             <span className="text-emerald-600 underline">Sign Up</span>
           </p>
-        <input
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
             className="outline-none text-center bg-transparent border-2 border-emerald-600 text-sm rounded-full placeholder:text-gray-400 py-3 px-10"
             type="email"
             placeholder="Enter your email"
           />
-           <input
+          <input
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
             className="outline-none text-center bg-transparent border-2 border-emerald-600 text-sm rounded-full placeholder:text-gray-400 mt-3 py-3 px-10"
@@ -34,7 +48,7 @@ const Login = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
